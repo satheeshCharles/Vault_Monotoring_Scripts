167c8f4672da0bc9c884e02fb158accad96a9a88
SatheeshCharles <satheesh.charles@gmail.com>
2018-12-20 16:50:59 +0800

    Create tls_certificate_verify.sh

---
+++ tls_certificate_verify.sh
@@ -0,0 +1,15 @@
+GET="vault.test.mydomain.com";
+RECIPIENT="satheesh.charles@gmail.com";
+DAYS=7;
+echo "checking if $TARGET expires in less than $DAYS days";
+expirationdate=$(date -d "$(: | openssl s_client -connect $TARGET:443 -servername $TARGET 2>/dev/null \
+                              | openssl x509 -text \
+                              | grep 'Not After' \
+                              |awk '{print $4,$5,$7}')" '+%s');
+in7days=$(($(date +%s) + (86400*$DAYS)));
+if [ $in7days -gt $expirationdate ]; then
+    echo "Vault - Certificate for $TARGET expires in less than $DAYS days, on $(date -d @$expirationdate '+%Y-%m-%d')" \
+    | mail -s "Certificate expiration warning for $TARGET" $RECIPIENT ;
+else
+    echo "OK - Certificate expires on $expirationdate";
+fi;
